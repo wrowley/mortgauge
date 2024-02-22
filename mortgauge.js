20cab4b952f930dace3b331b8f6c51465abfe061
wrowley <2756473+wrowley@users.noreply.github.com>
2024-02-22 17:58:08 +1100

    Adding index.html and mortgauge.js
    UTF-8 is obviously very good

---
+++ mortgauge.js
@@ -0,0 +1,9 @@
+//$(document).ready(function () {
+    for (var i=0; i<6; i++) {
+        var row = document.querySelector("table").insertRow();
+        for (var j=0; j<6; j++) {
+            row.insertCell().innerHTML = "👍 hello 👍";
+        }
+    }
+//}
+//)
