99d43a1227b33929cc4c5095cc0ce5f5f80a8b96
wrowley <2756473+wrowley@users.noreply.github.com>
2024-02-22 18:29:47 +1100

    add a price in $M and a conversion to $

--- mortgauge.js
+++ mortgauge.js
@@ -1,9 +1,16 @@
 //$(document).ready(function () {
-    for (var i=0; i<6; i++) {
-        var row = document.querySelector("table").insertRow();
-        for (var j=0; j<6; j++) {
-            row.insertCell().innerHTML = "👍 hello 👍";
-        }
+
+const total_millies = document.getElementById("total_millies");
+const total_dollies = document.getElementById("total_dollies");
+
+total_millies.addEventListener("input", function (e) {
+    total_dollies.value = e.target.value * 1000000;
+});
+
+//document.querySelector("table").innerHTML = '';
+for (var i=0; i<6; i++) {
+    var row = document.querySelector("table").insertRow();
+    for (var j=0; j<6; j++) {
+        row.insertCell().innerHTML = i*6+j;
     }
-//}
-//)
+}
